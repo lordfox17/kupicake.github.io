0c567ac3356c5d646a59faec7d95cf167d065a3b
kupicake <riskirw17@gmail.com>
2026-06-07 17:41:28 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+  const isGithubActions = process.env.GITHUB_ACTIONS === 'true' || process.env.GITHUB_PAGES === 'true';
   return {
-    base: '',
+    base: isGithubActions ? '/kupicake.github.io/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
